44a6ec93cb1321a30e68c87a7f67976521c853fd
sudoallenlopes23 <allenlopes10@gmail.com>
2023-10-07 03:10:20 +0530

    Commiting all files and folders!

---
+++ client/src/utils/index.js
@@ -0,0 +1,13 @@
+/* Utils file or utility file is a file where we can create different functions which we can use
+    later on across our application. */
+
+import { surpriseMePrompts } from "../constants";
+
+export function getRandomPrompt(prompt) {
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+  const randomPrompt = surpriseMePrompts[randomIndex];
+
+  if (randomPrompt === prompt) return getRandomPrompt(prompt);
+
+  return randomPrompt;
+}
